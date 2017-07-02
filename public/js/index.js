cd0c432dc804f21eebc2ef7b3f8b10a9d52e8453
Hyouk Seo <ghsspower@naver.com>
2017-07-02 23:55:28 +0900

    js

---
+++ public/js/index.js
@@ -0,0 +1,64 @@
+
+//top hover
+function topUp() {
+    "use strict";
+    
+    $('.fa-angle-up').addClass('topUp');
+    $('.fa-angle-up').removeClass('topUp2');
+}
+function topDn() {
+    "use strict";
+    
+    $('.fa-angle-up').addClass('topUp2');
+    $('.fa-angle-up').removeClass('topUp');
+}
+
+function nextRight() {
+    "use strict";
+    
+    $('.fa-angle-double-right').addClass('nextRight');
+    $('.fa-angle-double-right').removeClass('nextRight2');
+}
+function nextRightDel() {
+    "use strict";
+    
+    $('.fa-angle-double-right').addClass('nextRight2');
+    $('.fa-angle-double-right').removeClass('nextRight');
+}
+
+function nextLeft() {
+    "use strict";
+    
+    $('.fa-angle-double-left').addClass('nextLeft');
+    $('.fa-angle-double-left').removeClass('nextLeft2');
+}
+function nextLeftDel() {
+    "use strict";
+    
+    $('.fa-angle-double-left').addClass('nextLeft2');
+    $('.fa-angle-double-left').removeClass('nextLeft');
+}
+
+
+//nav hover
+function lineLive2() {
+    "use strict";
+    
+    $('.ready2').removeClass('aboutHover2');
+}
+function lineDel2() {
+    "use strict";
+    
+    $('.ready2').addClass('aboutHover2');
+}
+
+function lineLive3() {
+    "use strict";
+    
+    $('.ready3').removeClass('aboutHover3');
+}
+function lineDel3() {
+    "use strict";
+    
+    $('.ready3').addClass('aboutHover3');
+}
