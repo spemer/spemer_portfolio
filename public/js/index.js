cfd382701bff96c07772cfed1a974b3560d35dcb
Hyouk Seo <ghsspower@naver.com>
2018-01-12 16:40:17 +0900

    async pacs.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 //"use strict";
 
-//
-// pace.js
-(function(){
-    let headTitle = document.querySelector('head');
-    let setScript = document.createElement('script');
-    setScript.setAttribute('async','');
-    setScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js');
-    headTitle.appendChild(setScript);
-})();
+// //
+// // pace.js
+// (function(){
+//     let headTitle = document.querySelector('head');
+//     let setScript = document.createElement('script');
+//     setScript.setAttribute('async','');
+//     setScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js');
+//     headTitle.appendChild(setScript);
+// })();
 
 
 //
