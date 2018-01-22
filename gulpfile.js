6bae70cc00a8f58efb61ecad9b15145658431579
Hyouk Seo <ghsspower@naver.com>
2018-01-22 14:40:26 +0900

    delete gulp-inject

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,4 @@
 "use strict";
 const gulp = require('gulp');
-const htmlclean = require('gulp-htmlclean');
 const cleanCSS = require('gulp-clean-css');
 const minifyjs = require('gulp-js-minify');
-const inject = require('gulp-inject');
-
