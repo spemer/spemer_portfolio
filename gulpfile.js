1c3082e265555fbdc051cff81fc9238a60afc320
Hyouk Seo <ghsspower@naver.com>
2018-01-22 11:41:42 +0900

    css js minify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const gulp = require('gulp');
 //
 // css auto prefix
 const autoprefixer = require('gulp-autoprefixer');
-gulp.task('prefixer', () =>
+gulp.task('prefix', () =>
     gulp.src('public/css/*.css')
         .pipe(autoprefixer({
             browsers: ['last 99 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('public/css'))
+        .pipe(gulp.dest('public/tmp/css'))
 );
 
 //
@@ -21,5 +21,23 @@ gulp.task('cmq', function () {
         .pipe(cmq({
             log: true
         }))
-        .pipe(gulp.dest('public/css'));
+        .pipe(gulp.dest('public/tmp/css'));
+});
+
+//
+// css minify
+const cleanCSS = require('gulp-clean-css');
+gulp.task('mcss', () => {
+  return gulp.src('public/tmp/css/*css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('public/dist/css'));
+});
+
+//
+// js minify
+const minifyjs = require('gulp-js-minify');
+gulp.task('mjs', function(){
+  gulp.src('public/js/*.js')
+    .pipe(minifyjs())
+    .pipe(gulp.dest('public/dist/js'));
 });
