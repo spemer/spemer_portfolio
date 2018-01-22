99a0edfba88f2237997bcbb76e59713239eb9d6c
Hyouk Seo <ghsspower@naver.com>
2018-01-22 15:35:38 +0900

    delete cleancss

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ const minifyjs = require('gulp-js-minify');
 
 // minify
 const paths = {
-    src: 'public/js/index',
+    src: 'public',
     srcJS: 'public/js/index/*.js',
 
-    dist: 'public/js/dist',
-    distJS: 'public/js/dist/*.js'
+    dist: 'public',
+    distJS: 'public/js/dist'
 };
 gulp.task('js', function(){
     return gulp.src(paths.srcJS)
     .pipe(minifyjs())
-    .pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(paths.distJS));
 });
 gulp.task('min', ['js']);
