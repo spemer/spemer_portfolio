4102def57509ef53fa22813ed6660a5b9a2a33e2
spemer <ghsspower@naver.com>
2018-03-08 17:04:15 +0900

    jsmin -> beusable

--- public/js/index/beusable.js
+++ public/js/index/beusable.js
@@ -24,3 +24,24 @@
     
     getBody.appendChild(setBeusably);
 })();
+
+
+//
+// cdns
+// (function(){
+//     let cdnUrl = [
+//         "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+//         "https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.7/jquery.lazy.min.js",
+//         "https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.7/jquery.lazy.plugins.min.js",
+//         "https://cdnjs.cloudflare.com/ajax/libs/smooth-scroll/12.1.5/js/smooth-scroll.min.js"
+//     ]
+
+//     let getBody = document.querySelector('body');
+    
+//     for (let i = 0; i < cdnUrl.length; i++){
+//         let setScript = document.createElement('script');
+//         setScript.setAttribute('async', '');
+//         setScript.setAttribute('src', cdnUrl[i]);
+//         getBody.appendChild(setScript);
+//     }
+// })();
