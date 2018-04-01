f2c28c37218cd145ec64f4ad34806fb5da74dbc1
spemer <ghsspower@naver.com>
2018-04-02 03:28:37 +0900

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 "use strict";
 const gulp = require('gulp');
-const minifyjs = require('gulp-js-minify');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
 const watch = require('gulp-watch');
+const minifyjs = require('gulp-js-minify');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 
 
-// watch
-gulp.task('stream-css', function () {
-    // Endless stream mode
+/* ==============================
+    css autoprefixer -> tmp to dist
+============================== */
+gulp.task('prefix-css', function () {
     return watch('public/css/tmp/stylesheet.css', { ignoreInitial: false })
         .pipe(autoprefixer())
         .pipe(gulp.dest('public/css/dist'));
 });
 
-gulp.task('callback-js', function () {
-    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+
+/* ==============================
+    min js
+============================== */
+gulp.task('jsmin', function () {
     return watch('public/js/index/*.js', function () {
         gulp.src('public/js/index/*.js')
             .pipe(minifyjs())
@@ -23,37 +28,58 @@ gulp.task('callback-js', function () {
     });
 });
 
-// mini css
+
+/* ==============================
+    css minify
+============================== */
 gulp.task('mincss', () => {
     return gulp.src('public/css/*.css')
         .pipe(cleanCSS({compatibility: 'ie6'}))
         .pipe(gulp.dest('public/css/dist'));
 });
 
-gulp.task('watch', ['stream-css', 'callback-js', 'mincss'])
+
+/* ==============================
+    sass sync watch
+============================== */
+gulp.task('sass', function () {
+    return gulp.src('css/stylesheet.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('css/tmp'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('css/stylesheet.scss', ['sass']);
+});
+
+
+/* ==============================
+    watch tasks
+============================== */
+gulp.task('watch', ['prefix-css', 'jsmin', 'mincss', 'sass:watch'])
 
 
 
 // js minify
-const paths = {
-    src: 'public',
-    srcJS: 'public/js/index/*.js',
-    tmp: 'public',
-    tmpJS: 'public/js/tmp'
-};
-gulp.task('js', function(){
-    return gulp.src(paths.srcJS)
-    .pipe(minifyjs())
-    .pipe(gulp.dest(paths.distJS));
-});
-gulp.task('jsmin', ['js']);
+// const paths = {
+//     src: 'public',
+//     srcJS: 'public/js/index/*.js',
+//     tmp: 'public',
+//     tmpJS: 'public/js/tmp'
+// };
+// gulp.task('js', function(){
+//     return gulp.src(paths.srcJS)
+//     .pipe(minifyjs())
+//     .pipe(gulp.dest(paths.distJS));
+// });
+// gulp.task('jsmin', ['js']);
 
 
 // css prefix
-gulp.task('prefix', () =>
-    gulp.src('public/css/tmp/*.css')
-        .pipe(autoprefixer({
-            cascade: false
-    }))
-    .pipe(gulp.dest('public/css/dist'))
-);
+// gulp.task('prefix', () =>
+//     gulp.src('public/css/tmp/*.css')
+//         .pipe(autoprefixer({
+//             cascade: false
+//     }))
+//     .pipe(gulp.dest('public/css/dist'))
+// );
