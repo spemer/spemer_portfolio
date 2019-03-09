9ecbc04a66794017fc94842765c695cc85b58522
spemer <ghsspower@gmail.com>
2019-03-09 19:06:00 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# get commit message
+read -p "Enter commit message: " commitmsg
+
 # run on a local server with gulpfile.js
 # sudo gulp watch
 
 # deploy with firebase cli
-# firebase deploy
+firebase deploy
 
-# fullname="USER INPUT"
-read -p "Enter commit message: " commitmsg
 
 # commit
 git add .
