36b15a72adee91b33580dccbc2a77d5f9472014d
spemer <ghsspower@gmail.com>
2019-03-20 06:46:02 +0900

    FIX deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ firebase deploy
 
 # get commit message
 printf "\n"
-read -p "Enter commit message: " commitmsg
+IFS= read -r -p "Enter commit message: " commitmsg
 
 # commit
 git add .
-git commit -m ${commitmsg}
+git commit -m "$commitmsg"
 git push
 
 exit
