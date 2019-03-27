35c08f0c86b4a0096b230c225df59c31e10b89af
spemer <ghsspower@gmail.com>
2019-03-28 05:14:04 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
 
 echo "============================================================"
 echo "${BOLD}${PWD##*/}${RESET}"
@@ -12,7 +14,7 @@ echo "============================================================"
 run_gulpfile() {
   while true; do
     printf "\n"
-    read -p "${BOLD}Run gulpfile.js? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}Run gulpfile.js? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* )
         trap 'echo Stop gulp-watch' SIGINT
@@ -33,7 +35,7 @@ run_gulpfile() {
 firebase_deploy() {
   while true; do
     printf "\n"
-    read -p "${BOLD}firebase deploy? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}firebase deploy? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* ) firebase deploy; break;;
       [Nn]* ) return 0;;
@@ -48,7 +50,7 @@ firebase_deploy() {
 git_commit() {
   while true; do
     printf "\n"
-    read -p "${BOLD}git commit? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}git commit? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* )
         IFS= read -r -p "${BOLD}Enter commit message: ${RESET}" commitmsg
@@ -56,7 +58,7 @@ git_commit() {
         # if commitmsg empty
         if [ -z "$commitmsg" ]
         then
-          echo "${BOLD}Commit message is empty${RESET}"
+          echo "${BOLD}${RED}Commit message is empty${RESET}"
           commitmsg="Add files via upload"
         fi
 
@@ -77,7 +79,7 @@ git_commit() {
 git_push() {
   while true; do
     printf "\n"
-    read -p "${BOLD}git push? (Y/n)${RESET}" yn
+    read -p "${BOLD}${GREEN}git push? (Y/n) ${RESET}" yn
     case ${yn} in
       [Yy]* ) git push; break;;
       [Nn]* ) return 0;;
